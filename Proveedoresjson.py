e7ca88968acd9940ef09c61b19a6865e3bc8c111
Eduardo Batta González <36352244+Eduardo-Batta@users.noreply.github.com>
2018-05-16 07:11:46 -0500

    Create Proveedoresjson.py

---
+++ Proveedoresjson.py
@@ -0,0 +1,168 @@
+import json
+Proveedores = { ' Titulo ' : " Proveedores" , ' nombre ' : Proveedor1 , ' Correo electrónico ' : example@example.com,' dirección ' : calle-cp-numero , 0 : { ' dato ' : ' ejemplo1 ' , ' dato1 ' : 2 }}
+
+print (Proveedores)
+print (Proveedores [ ' nombre ' ])
+print (Proveedores [ 0 ])
+print (Proveedores [ 0 ] [ ' dato ' ])
+
+#para atributo en Proveedores:
+    print (atributo)
+    print (Proveedores [atributo])
+   # para atributo2 en Proveedores [ 0 ]:
+        print atributo2
+
+cadena = json.dumps (Proveedores)
+
+#Cadena de impresión
+
+f = abrir ( ' json.dat ' , ' w ' )
+
+f.write (cadena)
+
+f.close ()
+
+f2 = abrir ( ' json.dat ' , ' r ' )
+
+CadenaJson = f2.read ()
+
+print CadenaJson
+
+jsonRecuperado = json.loads (CadenaJson)
+
+imprimir jsonRecuperado [ ' nombre ' ]
+
+jsonRecuperado [ ' nombre ' ] = ' Proveedor# '
+jsonRecuperado [ ' correo' ] = int (jsonRecuperado [ '  ' ]) + 10
+f = abrir ( ' json.dat ' , ' w ' )
+
+cadena = json.dumps (jsonRecuperado)
+f.write (cadena)
+
+f.close ()
+import json
+Proveedores = { ' Titulo ' : " Proveedores" , ' nombre ' : Proveedor1 , ' Correo electrónico ' : example@example.com,' dirección ' : calle-cp-numero , 0 : { ' dato ' : ' ejemplo1 ' , ' dato1 ' : 2 }}
+
+print (Proveedores)
+print (Proveedores [ ' nombre ' ])
+print (Proveedores [ 0 ])
+print (Proveedores [ 0 ] [ ' dato ' ])
+
+#para atributo en Proveedores:
+    print (atributo)
+    print (Proveedores [atributo])
+   # para atributo2 en Proveedores [ 0 ]:
+        print atributo2
+
+cadena = json.dumps (Proveedores)
+
+#Cadena de impresión
+
+f = abrir ( ' json.dat ' , ' w ' )
+
+f.write (cadena)
+
+f.close ()
+
+f2 = abrir ( ' json.dat ' , ' r ' )
+
+CadenaJson = f2.read ()
+
+print CadenaJson
+
+jsonRecuperado = json.loads (CadenaJson)
+
+imprimir jsonRecuperado [ ' nombre ' ]
+
+jsonRecuperado [ ' nombre ' ] = ' Proveedor# '
+jsonRecuperado [ ' correo' ] = int (jsonRecuperado [ '  ' ]) + 10
+f = abrir ( ' json.dat ' , ' w ' )
+
+cadena = json.dumps (jsonRecuperado)
+f.write (cadena)
+
+f.close ()
+import json
+Proveedores = { ' Titulo ' : " Proveedores" , ' nombre ' : Proveedor1 , ' Correo electrónico ' : example@example.com,' dirección ' : calle-cp-numero , 0 : { ' dato ' : ' ejemplo1 ' , ' dato1 ' : 2 }}
+
+print (Proveedores)
+print (Proveedores [ ' nombre ' ])
+print (Proveedores [ 0 ])
+print (Proveedores [ 0 ] [ ' dato ' ])
+
+#para atributo en Proveedores:
+    print (atributo)
+    print (Proveedores [atributo])
+   # para atributo2 en Proveedores [ 0 ]:
+        print atributo2
+
+cadena = json.dumps (Proveedores)
+
+#Cadena de impresión
+
+f = abrir ( ' json.dat ' , ' w ' )
+
+f.write (cadena)
+
+f.close ()
+
+f2 = abrir ( ' json.dat ' , ' r ' )
+
+CadenaJson = f2.read ()
+
+print CadenaJson
+
+jsonRecuperado = json.loads (CadenaJson)
+
+imprimir jsonRecuperado [ ' nombre ' ]
+
+jsonRecuperado [ ' nombre ' ] = ' Proveedor# '
+jsonRecuperado [ ' correo' ] = int (jsonRecuperado [ '  ' ]) + 10
+f = abrir ( ' json.dat ' , ' w ' )
+
+cadena = json.dumps (jsonRecuperado)
+f.write (cadena)
+
+f.close ()
+import json
+Proveedores = { ' Titulo ' : " Proveedores" , ' nombre ' : Proveedor1 , ' Correo electrónico ' : example@example.com,' dirección ' : calle-cp-numero , 0 : { ' dato ' : ' ejemplo1 ' , ' dato1 ' : 2 }}
+
+print (Proveedores)
+print (Proveedores [ ' nombre ' ])
+print (Proveedores [ 0 ])
+print (Proveedores [ 0 ] [ ' dato ' ])
+
+#para atributo en Proveedores:
+    print (atributo)
+    print (Proveedores [atributo])
+   # para atributo2 en Proveedores [ 0 ]:
+        print atributo2   
+
+cadena = json.dumps (Proveedores)
+    
+#Cadena de impresión
+
+f = abrir ( ' json.dat ' , ' w ' )
+
+f.write (cadena)
+
+f.close ()
+
+f2 = abrir ( ' json.dat ' , ' r ' )
+
+CadenaJson = f2.read ()
+
+print CadenaJson
+
+jsonRecuperado = json.loads (CadenaJson)
+
+imprimir jsonRecuperado [ ' nombre ' ]
+
+jsonRecuperado [ ' nombre ' ] = ' Proveedor# '
+jsonRecuperado [ ' correo' ] = int (jsonRecuperado [ '  ' ]) + 10
+f = abrir ( ' json.dat ' , ' w ' )
+
+cadena = json.dumps (jsonRecuperado)
+f.write (cadena)
+
+f.close ()
